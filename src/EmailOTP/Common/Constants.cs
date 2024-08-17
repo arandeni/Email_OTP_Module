bbe94316c066fcd6d04315883acead31b0136701
Achini Randeni <achinirandeni@gmail.com>
2024-08-17 09:14:53 +0800

    Added Constants changes

--- src/EmailOTP/Common/Constants.cs
+++ src/EmailOTP/Common/Constants.cs
@@ -2,5 +2,19 @@
 {
     public class Constants
     {
+        public const string STATUS_EMAIL_OK = "email containing OTP has been sent successfully";
+        public const string STATUS_EMAIL_FAIL = "email address does not exist or sending to the email has failed";
+        public const string STATUS_EMAIL_INVALID = "email address is invalid";
+        public const string STATUS_EMAIL_OTP_SENT = "OTP has been already sent to this email address";
+        public const string STATUS_OTP_OK = "OTP is valid and checked";
+        public const string STATUS_OTP_INVALID = "OTP is invalid";
+        public const string STATUS_OTP_INVALID_EMAIL = "email is invalid";
+        public const string STATUS_OTP_FAIL = "OTP is wrong after 10 tries";
+        public const string STATUS_OTP_TIMEOUT = "timeout after 1 min";
+        public const string STATUS_OTP_SESSION_EXPIRED = "session has been expired";
+        public const string INTERNAL_SERVER_ERROR = "Error occurred while processing your request";
+        public const int MAX_ATTEMPTS = 10;
+        public const int OTP_VALIDITY = 60;
+        public const string DOMAIN = "dso.org.sg";
     }
 }
