6813ec9b0cc7c02ac0aeb325fb4a110052f01aec
Achini Randeni <achinirandeni@gmail.com>
2024-08-17 09:17:41 +0800

    Added EmailService

---
+++ src/EmailOTP/Services/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmailOTP.Services
+{
+    public class EmailService : IEmailService
+    {
+        /// <summary>
+        /// Validate the email address
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public bool IsEmailValid(string email)
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+        }
+
+        /// <summary>
+        /// Assume this function is implemented to send an email
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <param name="emailBody"></param>
+        /// <returns></returns>
+        public bool send_email(string email_address, string email_body)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/EmailOTP/Services/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace EmailOTP.Services
+{
+    public interface IEmailService
+    {
+        bool send_email(string email_address, string email_body);
+        bool IsEmailValid(string email);
+    }
+}
