41179fb6ac11a832d4eee2df8e1c1fe927242e92
Achini Randeni <achinirandeni@gmail.com>
2024-08-17 09:16:03 +0800

    Added OTPDetails

---
+++ src/EmailOTP/Models/OTPDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailOTP.Models
+{
+    public class OTPDetails
+    {
+        public string Email { get; set; }
+        public string OTP { get; set; }
+        public DateTime GeneratedTime { get; set; }
+        public int Attempts { get; set; }
+    }
+}
